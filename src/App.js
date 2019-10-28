29ad347342351f38765be03123a591a1f4e83e35
Lorena <lorena.tacal@gmail.com>
2019-10-28 23:50:29 +0000

    Add StyledComponents to the project

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledImage = styled.img`
+  width: 10%;
+  height: 10%;
+`
+const Container = styled.div`
+  margin: 10%;
+  text-align: center;
+  border: 0.1px solid grey;
+`
+const ImageDiv = styled.div`
+  margin-top: 2%
+`
+const Won = styled.img`
+  width: 30%;
+  height: 30%;
+`
+const StyledButton = styled.button`
+  display: inline-block;
+  color: palevioletred;
+  font-size: 1.1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`
+const StyledInput = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: white;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  font-size: 1em
+`
+const Title = styled.h1`
+  font-size: 2em;
+  color: black;
+`
+const Text = styled.p`
+  font-size: 1.3em;
+`
+
 function App() {
   const [numberToBeGuessed] = React.useState(Math.round(Math.random()*10000));
   console.log(numberToBeGuessed)
   const [userGuessedNumber, setUserGuessedNumber] = React.useState([])
   const [typedInput, setTypedInput] = React.useState('')
 
-  const checkWinner = () => {
-    if(userGuessedNumber === numberToBeGuessed) {
-      return(
-        <p>You won</p>
-      )
-    }
-  }
-
   return (
-    <>
-      <h1>Play Bull and Cows</h1>
+    <Container>
+      <Title>Play Bull and Cows</Title>
       {/* <p>Number to be guessed: {numberToBeGuessed}</p> */}
-      <p>Your choice: {userGuessedNumber}</p>
-      <input onChange={event => setTypedInput(event.target.value)}></input>
-      <button 
+      <Text>Your choice: {userGuessedNumber}</Text>
+      <StyledInput onChange={event => setTypedInput(event.target.value)} />
+      <StyledButton 
         onClick={() =>  {setUserGuessedNumber(typedInput)}}
       >Submit your Answer
-      </button>
+      </StyledButton>
       <Result answer={numberToBeGuessed} guess={userGuessedNumber} />
       {/* {userGuessedNumber === numberToBeGuessed && <p>You won</p>} */}
-    </>
+    </ Container>
   );
 }
 
@@ -37,7 +72,7 @@ function Result({ answer, guess }) {
 
   for(let i=0; i<4; i++) {
     if(answerGuessed.includes(answerArray[i])){
-      if(answerArray[i] == answerGuessed[i]) {
+      if(answerArray[i] === answerGuessed[i]) {
         bulls = bulls + 1;
       } else {
         cows = cows + 1
@@ -46,11 +81,20 @@ function Result({ answer, guess }) {
   }
   if(bulls === 4) {
     return(
-      <h1>You won!</h1>
+      <ImageDiv>
+      <Won src = "http://halopublishing.com/blog/wp-content/uploads/2015/05/winner.jpg" />
+      </ImageDiv>
     )
   } else {
     return (
-      `${bulls} bulls and ${cows} cows.  TRY AGAIN!`)
+      <>
+      <Text>{bulls} bulls and</Text> 
+      <StyledImage src="https://i.dlpng.com/static/png/1177844-charging-bull-cattle-clip-art-bull-png-transparent-images-cow-and-bull-png-900_520_preview.png" />
+      <Text>{cows} cows</Text>
+      <StyledImage src="https://www.fourjay.org/myphoto/f/45/453681_cows-png.jpg" />
+      <h1>Try Again!</h1>
+      </>
+    )
   }
 }
 
