331bae4386ada5aece798443239294356929cd42
Lorena <lorena.tacal@gmail.com>
2019-10-28 16:50:37 +0000

    Add Cow and Bulls functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const [numberToBeGuessed] = React.useState(Math.round(Math.random()*10000));
+  console.log(numberToBeGuessed)
+  const [userGuessedNumber, setUserGuessedNumber] = React.useState([])
+  const [typedInput, setTypedInput] = React.useState('')
+
+  const checkWinner = () => {
+    if(userGuessedNumber === numberToBeGuessed) {
+      return(
+        <p>You won</p>
+      )
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Play Bull and Cows</h1>
+      {/* <p>Number to be guessed: {numberToBeGuessed}</p> */}
+      <p>Your choice: {userGuessedNumber}</p>
+      <input onChange={event => setTypedInput(event.target.value)}></input>
+      <button 
+        onClick={() =>  {setUserGuessedNumber(typedInput)}}
+      >Submit your Answer
+      </button>
+      <Result answer={numberToBeGuessed} guess={userGuessedNumber} />
+      {/* {userGuessedNumber === numberToBeGuessed && <p>You won</p>} */}
+    </>
   );
 }
 
+function Result({ answer, guess }) {
+  const answerArray = answer.toString().split('');
+  const answerGuessed = guess.toString().split('');
+  let cows = 0;
+  let bulls = 0;
+
+  for(let i=0; i<4; i++) {
+    if(answerGuessed.includes(answerArray[i])){
+      if(answerArray[i] == answerGuessed[i]) {
+        bulls = bulls + 1;
+      } else {
+        cows = cows + 1
+      }
+    }
+  }
+  if(bulls === 4) {
+    return(
+      <h1>You won!</h1>
+    )
+  } else {
+    return (
+      `${bulls} bulls and ${cows} cows.  TRY AGAIN!`)
+  }
+}
+
 export default App;
+
+
+
